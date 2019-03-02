f282e8bb9f39c33a1168c90dad5928b35f2b8d2a
cjacobme <cjacobme@aol.com>
2019-03-02 18:14:10 +0100

    correct forecast timestamp

--- CJ  -  Cassandra Editor/src/main/java/cj/software/cassandra/migration/IterateNotMigrated.java
+++ CJ  -  Cassandra Editor/src/main/java/cj/software/cassandra/migration/IterateNotMigrated.java
@@ -1,12 +1,16 @@
 package cj.software.cassandra.migration;
 
+import java.time.Duration;
 import java.time.Instant;
 
 import org.apache.log4j.Logger;
 
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Cluster.Builder;
 import com.datastax.driver.core.CodecRegistry;
+import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
@@ -49,6 +53,27 @@ public class IterateNotMigrated
 
 	private void iterate(Session pSession)
 	{
+		PreparedStatement lPrepInsert = pSession.prepare(
+				"INSERT INTO marktdaten "
+						+ "(service_provider, lieferzone, marktpreiskurve, von, bis, "
+						+ "prognose, migrated, wert)  "
+						+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+		PreparedStatement lPrepDelete = pSession.prepare(
+				"DELETE FROM marktdaten "
+						+ "WHERE service_provider = ? "
+						+ "AND lieferzone = ? "
+						+ "AND marktpreiskurve = ? "
+						+ "AND von = ? "
+						+ "AND bis = ? "
+						+ "AND prognose = ?");
+		this.iterate(pSession, lPrepDelete, lPrepInsert);
+	}
+
+	private void iterate(
+			Session pSession,
+			PreparedStatement pPrepDelete,
+			PreparedStatement pPrepInsert)
+	{
 		String lQuery = "SELECT service_provider, lieferzone, marktpreiskurve, von, bis, prognose, "
 				+ "false_if_null(migrated) as migrated, "
 				+ "wert "
@@ -65,6 +90,41 @@ public class IterateNotMigrated
 							lPrognose,
 							lWert,
 							String.valueOf(lMigrated)));
+			if (!lMigrated)
+			{
+				String lServiceProvider = bRow.getString("service_provider");
+				String lLieferzone = bRow.getString("lieferzone");
+				String lMarktpreisekurve = bRow.getString("marktpreiskurve");
+				Instant lVon = bRow.get("von", Instant.class);
+				Instant lBis = bRow.get("bis", Instant.class);
+
+				Instant lPrognoseCorrected = lPrognose.minus(Duration.ofDays(1));
+				BatchStatement lBatch = new BatchStatement();
+				BoundStatement lBoundInsert = pPrepInsert.bind(
+						lServiceProvider,
+						lLieferzone,
+						lMarktpreisekurve,
+						lVon,
+						lBis,
+						lPrognoseCorrected,
+						true,
+						lWert);
+				lBatch.add(lBoundInsert);
+				BoundStatement lBoundDelete = pPrepDelete.bind(
+						lServiceProvider,
+						lLieferzone,
+						lMarktpreisekurve,
+						lVon,
+						lBis,
+						lPrognose);
+				lBatch.add(lBoundDelete);
+				pSession.execute(lBatch);
+				this.logger.info(
+						String.format(
+								"corrected prognose from %s to %s",
+								lPrognose,
+								lPrognoseCorrected));
+			}
 		}
 	}
 }
